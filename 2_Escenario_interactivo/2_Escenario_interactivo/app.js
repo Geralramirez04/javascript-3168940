197234938da602a20fc1f84ebe68572d704b99b9
Geral <geralramirezc2004@gmail.com>
2025-10-22 17:25:51 -0500

    segundo escenario

--- 2_Escenario_interactivo/2_Escenario_interactivo/app.js
+++ 2_Escenario_interactivo/2_Escenario_interactivo/app.js
@@ -1,12 +1,36 @@
-const Tulipan = document.querySelectorAll(".Tulipan");
-const Mariposa= document.querySelector(".Mariposa");
-let contadorTulipan = 0;
-  let contadorMariposa = 0;
+const hongo1 = document.getElementById ("hongo1");
+const hongo2 = document.getElementById ("hongo2");
+const hongo3 = document.getElementById ("hongo3");
+const hongo4 = document.getElementById ("hongo4");
+const Estrella = document.getElementById ("Estrella");
+const burbuja = document.getElementById ("burbuja");
+const contadorNumero = document.getElementById("contador-numero");
+const contador = document.getElementById("contador");
 
-     Tulipan.forEach(item => {
+let totalClicks = 0;
 
-     item.addEventListener('click', () => {
-      item.style.filter = 'grayscale(1)'; 
-      item.classList.add('saltar');       
-     });
-     });
+function sumarContador() {
+  totalClicks++;
+  contadorNumero.textContent = totalClicks;
+}
+
+hongo1.addEventListener("click", sumarContador);
+hongo2.addEventListener("click", sumarContador);
+hongo3.addEventListener("click", sumarContador);
+hongo44.addEventListener("click", sumarContador);
+
+hongo1.addEventListener("click", () => {
+    hongo1.classList.add("desaparecer");
+  });
+
+hongo2.addEventListener("click", () => {
+    hongo2.classList.add("desaparecer");
+  });
+
+hongo3.addEventListener("click", () => {
+    hongo3.classList.add("desaparecer");
+  });
+
+hongo4.addEventListener("click", () => {
+    hongo4.classList.add("desaparecer");
+  });
